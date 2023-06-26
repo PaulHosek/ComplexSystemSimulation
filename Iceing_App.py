2832bb296921ed3fabf3c4c251ff91df31b4bfee
SesswireD <dlbezema@gmail.com>
2023-06-26 15:52:16 +0200

    Working widget

--- Iceing_App.py
+++ Iceing_App.py
@@ -4,10 +4,24 @@ import plotly.express as px
 import numpy as np
 from iceing import iceing_model
 from iceing import glauber
+from iceing import perim_area
+from matplotlib import colors
 
+st.set_page_config(
+    page_title="Iceing Model",
+    page_icon="❄️",
+    layout="wide",
+)
+
+cmap = colors.ListedColormap(['blue', 'white'])
+bounds=[-100,0,100]
+norm = colors.BoundaryNorm(bounds, cmap.N)
+
+#set column containers for the graphs
+col1, col2 = st.columns(2)
 
 #set title and subtitle
-st.header('Iceing Model')
+st.title('Iceing Model')
 st.write(
     """ 
     This demo shows the formation of meltponds on arctic sea ice during the summer period using an adaptation from the Ising model.
@@ -29,33 +43,39 @@ with st.sidebar:
 if st.session_state.initialize:
     with st.spinner('Initialize domain..'):
         #initialize model
-        st.session_state.model = iceing_model(input_fraction,50)
+        st.session_state.model = iceing_model(input_fraction,300)
 
         #display initial topology
-        st.session_state.fig,st.session_state.ax = plt.subplots(figsize=(10,10))
-        plt.imshow(st.session_state.model.s)
+        st.session_state.fig,st.session_state.ax = plt.subplots(figsize=(5,5))
+
+        plt.imshow(st.session_state.model.s,cmap=cmap,norm=norm)
         st.pyplot(st.session_state.fig)
 
         
 #Run the simulation once start button is clicked
 if st.session_state.start_sim_clicked:
     with st.spinner("Running Simulation"):
+            
+            #stop simulation once stop button is clicked
+            with st.empty():
+                while not st.session_state.stop_sim_clicked:
 
-        #stop simulation once stop button is clicked
-        while not st.session_state.stop_sim_clicked:
+                    #run the simulation for x timesteps
+                    new_state,sums = glauber(st.session_state.model.s,st.session_state.model.hi,1000)
+                    st.session_state.model.s = new_state
 
-            #run the simulation for x timesteps
-            new_state,sums = glauber(st.session_state.model.s,st.session_state.model.hi,10000)
+                    #display new state
+                    st.session_state.fig,st.session_state.ax = plt.subplots(figsize=(5,5))
+                    plt.imshow(st.session_state.model.s,cmap=cmap,norm=norm)
+                    st.pyplot(st.session_state.fig,clear_figure=True)
 
-            st.session_state.model.s = new_state
-
-            #display new state
-            st.session_state.fig,st.session_state.ax = plt.subplots(figsize=(10,10))
-            plt.imshow(st.session_state.model.s)
-            st.pyplot(st.session_state.fig)
+                    # #calculate the perim area and plot
+                    # areas, perimeters = perim_area(new_state, pond_val = -1, ice_val = 1)
+                    # print(areas,perimeters)
+                    # st.pyplot(plt.scatter(areas, perimeters))
 
     #plot latest state
-    st.session_state.fig,st.session_state.ax = plt.subplots(figsize=(10,10))
-    plt.imshow(st.session_state.model.s)
+    st.session_state.fig,st.session_state.ax = plt.subplots(figsize=(5,5))
+    plt.imshow(st.session_state.model.s,cmap=cmap,norm=norm)
     st.pyplot(st.session_state.fig)
 
--- iceing.py
+++ iceing.py
@@ -1,5 +1,8 @@
 import numpy as np
 from numba import njit
+from scipy.ndimage import label
+
+
 
 @njit
 def glauber(s = np.array, hi = np.array, N = int):
@@ -47,6 +50,48 @@ def glauber(s = np.array, hi = np.array, N = int):
             sums[int(i/1_000_000)] = np.sum(s) 
     
     return s, sums
+
+def perim_area(ponds, pond_val = -1, ice_val = 1):
+    '''
+    Calculates the area and the perimeter of the meltpods
+
+    Arguments:
+        ponds -- 2D ndarray
+
+    Keyword Arguments:
+        pond_val -- value indicating that cell is part of a melt pond (default: {-1})
+        ice_val --  value indicating that cell is ice (default: {1})
+
+    Returns:
+        areas -- 1D ndarray with the area of each melt pond
+        perimeters -- 1D ndarray with the perimeter of each melt pond
+    '''
+
+    # get the pond clusters
+    pond_clusters, _ = label(np.where((ponds) == pond_val, 1, 0))
+
+    clusters = np.unique(pond_clusters)[1:]
+    areas = np.zeros(len(clusters))
+    perimeters = np.zeros(len(clusters))
+    
+    for i, id in enumerate(clusters):
+        areas[i] = np.sum(pond_clusters == id)
+
+        # get the total number of neighboring cells for each cluster
+        for cell in np.transpose(np.nonzero(pond_clusters == id)):
+            row, col = cell
+
+            perimeters[i] += np.sum(np.array([
+                        ponds[row+1, col] == ice_val,
+                        ponds[row-1, col] == ice_val,
+                        ponds[row, col+1] == ice_val,
+                        ponds[row, col-1] == ice_val,
+                    ]))
+
+
+
+    return areas, perimeters
+
 
 class iceing_model:
 
