8e9f4621ca5dd4a65fd01bc37755850710b5b36b
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2023-06-29 11:24:25 +0200

    GitHub structuring

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='ComplexSystemSimulation',
     version='1.0.0',
     packages=[''],
     url='',
     license='',
-    description='Artic ice melt ponds Cellular Automata (CA) model'
+    description='Artic ice melt ponds Cellular Automata (CA) model',
+    install_requires = requirements
 )
