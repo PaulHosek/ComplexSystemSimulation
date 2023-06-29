b751a208e3221ca9bcd389ed32f90505a0241121
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2023-06-29 09:12:04 +0200

    Inflection point function.

--- evaluation.py
+++ evaluation.py
@@ -2,19 +2,6 @@ import numpy as np
 import scipy.ndimage as ndimage
 from numba import njit
 from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-import os
-from matplotlib import colors
-import re
-import initial_distributions
-import scipy.stats as stats
-
-# make a color map of fixed colors
-cmap = colors.ListedColormap(['cyan', 'white', 'blue'])
-bounds = [-100, 0.5, 3, 100]
-norm = colors.BoundaryNorm(bounds, cmap.N)
-
 
 def perim_area(ponds, pond_val=-1, ice_val=1):
     '''
@@ -79,15 +66,15 @@ def detect_percolation(grid, sidelength):
     labels, num = ndimage.label(grid)
     area = ndimage.sum(grid, labels, index=np.arange(labels.max() + 1))
     unique, counts = np.unique(labels, return_counts=True)
-    cluster_candidates = np.asarray((unique, counts)).T[1:]  # index, area array
+    cluster_candidates = np.asarray((unique, counts)).T[1:] # index, area array
     cluster_candidates = cluster_candidates[cluster_candidates[:, 1] >= sidelength]
-    cluster_candidates = cluster_candidates[cluster_candidates[:, 1].argsort()][::-1]
+    cluster_candidates = cluster_candidates[cluster_candidates[:,1].argsort()][::-1]
 
     # Create dict with {cluster_idx: set of edges}
     top = np.unique(labels[0])
     right = np.unique(labels[:, -1])
     bottom = np.unique(labels[-1])
-    left = np.unique(labels[:, 0])
+    left = np.unique(labels[:,0])
     cluster_edge = {}
     for border_idx, clusters in enumerate([top, right, bottom, left], start=1):
         clusters = set(clusters[clusters != 0])
@@ -95,17 +82,127 @@ def detect_percolation(grid, sidelength):
             cluster_edge.setdefault(c, set()).add(border_idx)
 
     # Iterate over clusters from largest to smallest and test if perturbation occurs.
-    for cluster_id, _ in cluster_candidates:
-        edges = cluster_edge.get(cluster_id, set())
-        if edges.issuperset({1, 3}) or edges.issuperset({2, 4}):
-            return True, np.ma.masked_where(labels != cluster_id, labels)
+    for cluster_id,_ in cluster_candidates:
+        edges = cluster_edge.get(cluster_id,set())
+        if edges.issuperset({1,3}) or edges.issuperset({2,4}):
+            return True, np.ma.masked_where(labels != cluster_id,labels)
 
     return False, None
 
+def fractal_dim(ponds, pond_val=-1, ice_val=1, bins = 50, min_area = 0):
 
-def fractal_dim(ponds, pond_val=-1, ice_val=1, bins=50, min_area=0):
+    # get areas and perimeters
+    areas, perimeters = perim_area(ponds, pond_val = pond_val, ice_val = ice_val)
+
+    # sort arrays
+    areas, perimeters = zip(*sorted(zip(areas, perimeters)))
+    areas = np.array(areas)
+    perimeters = np.array(perimeters)[areas >= min_area]
+    areas = areas[areas >= min_area]
+
+    # bin data and get the lowest perimeter for fitting
+    areas, perimeters = get_lowest(areas, perimeters, bins = bins)
+
+    # Perform curve fitting
+    fit_params, pcov = curve_fit(integral_D, np.log10(areas), np.log10(perimeters), p0=None)
+
+    # calculate the expected values
+    y_expect = D(np.log10(areas),*fit_params[:4])
+
+    if len(areas > 7):
+        Dims = []
+        for i in range(3, len(areas)-3):
+            dim = 2* (np.log(perimeters[i+3])-np.log(perimeters[i-3]))/(np.log(areas[i+3])-np.log(areas[i-3]))
+            Dims.append(dim)
+        
+        return areas, y_expect, pcov, areas[3:-3], np.array(Dims)
+    
+    else:
+        return areas, y_expect, pcov, np.array([]), np.array([])
+
+# Define the function D(x) and its integral
+def integral_D(x, a1, a2, a3, a4, a5):
+    return (a1 / (2 * a2)) * np.log(np.cosh(a2 * (x - a3))) + (a4 / 2) * x + a5
+
+def D(x, a1, a2, a3, a4):
+    return a1 * np.tanh(a2 * (x - a3)) + a4
+
+def get_lowest(areas_sorted, perimeters_sorted, bins=100):
+
+    _ , area_bins = np.histogram(np.log10(areas_sorted), bins = bins)
+    areas_binned = []
+    min_perimeters = []
+
+    for low, high in zip(area_bins[:-1], area_bins[1:]):
+
+        # Filter the sorted perimeters based on the current bin's area range
+        filtered_perimeters = perimeters_sorted[(np.log10(areas_sorted) >= low) & (np.log10(areas_sorted) < high)]
+
+        # Check if the filtered perimeters array is non-empty
+        if filtered_perimeters.size > 0:
+            # Calculate the minimum perimeter for the current bin
+            min_perimeter = np.min(filtered_perimeters)
+            # Calculate the mean area for the current bin
+            bin_area = np.mean([10**low, 10**high])
+            areas_binned.append(bin_area)
+            min_perimeters.append(min_perimeter)
+
+    return np.array(areas_binned), np.array(min_perimeters)
+
+
+def bootstrapping(ponds, pond_val=-1, ice_val=1, num_bootstrap=100):
+    """
+    This function returns a list of areas and perimeters after bootstrapping has been applied.
+    :param ponds:
+    :param pond_val:
+    :param ice_val:
+    :param num_bootstrap:
+    :return:
+    """
+
     # get areas and perimeters
     areas, perimeters = perim_area(ponds, pond_val=pond_val, ice_val=ice_val)
+
+    # Bootstrap resampling
+    areas_bootstrap = []
+    perimeters_bootstrap = []
+
+    for _ in range(num_bootstrap):
+        indices = np.random.choice(len(areas), size=len(areas), replace=True)
+        areas_sampled = areas[indices]
+        perimeters_sampled = perimeters[indices]
+
+        areas_bootstrap.append(areas_sampled)
+        perimeters_bootstrap.append(perimeters_sampled)
+
+    return areas_bootstrap, perimeters_bootstrap
+
+
+def inv_D(y, a1, a2, a3, a4):
+    """
+    This function returns the inverse of D.
+    :param y:
+    :param a1:
+    :param a2:
+    :param a3:
+    :param a4:
+    :return:
+    """
+    arg = (y-a4)/a1
+
+    x = 10**np.arctanh(arg/a2) + 10**a3
+
+    return x
+
+def fractal_dim_from_ap(areas, perimeters, bins = 50, min_area=0):
+    """
+    This funtion returns, if exists, the inflection point for a given list of areas and perimeters.
+    :param areas:
+    :param perimeters:
+    :param bins:
+    :param min_area:
+    :return:
+    """
 
     # sort arrays
     areas, perimeters = zip(*sorted(zip(areas, perimeters)))
@@ -120,10 +217,14 @@ def fractal_dim(ponds, pond_val=-1, ice_val=1, bins=50, min_area=0):
         # Perform curve fitting
         fit_params, pcov = curve_fit(integral_D, np.log10(areas), np.log10(perimeters), p0=None)
 
+        plot_areas = 10**np.linspace(0,5,1000)
         # calculate the expected values
-        y_expect = D(np.log10(areas), *fit_params[:4])
+        y_expect = D(np.log10(plot_areas), *fit_params[:4])
+
+        point = (np.max(y_expect) + np.min(y_expect))/2
+
     except:  # RuntimeError:
-        return np.array([]), np.array([]), np.array([]), np.array([]), np.array([])
+        return None
 
     if len(areas > 7):
         Dims = []
@@ -132,164 +233,26 @@ def fractal_dim(ponds, pond_val=-1, ice_val=1, bins=50, min_area=0):
                         np.log(areas[i + 3]) - np.log(areas[i - 3]))
             Dims.append(dim)
 
-        return areas, y_expect, pcov, areas[3:-3], np.array(Dims)
-
+        return point
     else:
-        return areas, y_expect, pcov, np.array([]), np.array([])
+        return None
 
 
-# Define the function D(x) and its integral
-def integral_D(x, a1, a2, a3, a4, a5):
-    return (a1 / (2 * a2)) * np.log(np.cosh(a2 * (x - a3))) + (a4 / 2) * x + a5
+def mean_inflation_pont(ponds):
+    """
+    This function calculates the average mean inflection value.
+    :param ponds:
+    :return: mean inflection value.
+    """
 
+    areas_bootstrap, perimeters_bootstrap = bootstrapping(ponds,-1,1, 100)
 
-def D(x, a1, a2, a3, a4):
-    return a1 * np.tanh(a2 * (x - a3)) + a4
+    inflection_list = []
 
+    for i in range(100):
+        point = fractal_dim_from_ap(areas_bootstrap[i],perimeters_bootstrap[i], 50, 0)
 
-def get_lowest(areas_sorted, perimeters_sorted, bins=100):
-    _, area_bins = np.histogram(np.log10(areas_sorted), bins=bins)
-    areas_binned = []
-    min_perimeters = []
+        if point != None:
+            inflection_list.append(point)
 
-    for low, high in zip(area_bins[:-1], area_bins[1:]):
-
-        # Filter the sorted perimeters based on the current bin's area range
-        filtered_perimeters = perimeters_sorted[(np.log10(areas_sorted) >= low) & (np.log10(areas_sorted) < high)]
-
-        # Check if the filtered perimeters array is non-empty
-        if filtered_perimeters.size > 0:
-            # Calculate the minimum perimeter for the current bin
-            min_perimeter = np.min(filtered_perimeters)
-            # Calculate the mean area for the current bin
-            bin_area = np.mean([10 ** low, 10 ** high])
-            areas_binned.append(bin_area)
-            min_perimeters.append(min_perimeter)
-
-    return np.array(areas_binned), np.array(min_perimeters)
-
-
-# Define a function to extract the numeric part of the filename
-def extract_number(filename):
-    match = re.search(r"_i=(\d+)", filename)
-    if match:
-        return int(match.group(1))
-    return -1
-
-
-def make_plots(experiment_name, threshold=0.01):
-    # create figure folders
-    if not os.path.exists(f"experiments/{experiment_name}/figures/"):
-        os.mkdir(f"experiments/{experiment_name}/figures")
-
-    h_filenames = sorted(os.listdir(f"experiments/{experiment_name}/pond"), key=extract_number)
-    H_filenames = sorted(os.listdir(f"experiments/{experiment_name}/ice"), key=extract_number)
-
-    ice_fraction = []
-    pond_fraction = []
-    ocean_fraction = []
-
-    for run in zip(h_filenames, H_filenames):
-        h = np.load(f"experiments/{experiment_name}/pond/{run[0]}")
-        H = np.load(f"experiments/{experiment_name}/ice/{run[1]}")
-
-        plot_array = np.where(H > 0, 1, 5)
-        plot_array = np.where(h > threshold, 0, plot_array)
-
-        ice_fraction.append(np.sum(plot_array == 1) / len(plot_array) ** 2)
-        pond_fraction.append(np.sum(plot_array == 0) / len(plot_array) ** 2)
-        ocean_fraction.append(np.sum(plot_array == 5) / len(plot_array) ** 2)
-
-        if pond_fraction[-1] > 0:
-            areas_dim, dimensions, _, areas_scatter, dimensions_scatter = fractal_dim(np.where(plot_array == 0, -1, 1),
-                                                                                      -1, 1, 50)
-
-        plt.clf()
-        # define figure layout
-        fig = plt.figure(figsize=(12, 6))
-        gs = GridSpec(2, 4, figure=fig)
-        ax1 = fig.add_subplot(gs[:, 0:2])
-        ax2 = fig.add_subplot(gs[0, 2:4])
-        ax3 = fig.add_subplot(gs[1, 2:4])
-
-        ax1.imshow(plot_array, cmap=cmap, norm=norm)
-
-        ax2.plot(ice_fraction, label='ice')
-        ax2.plot(pond_fraction, label='pond')
-        ax2.plot(ocean_fraction, label='ocean')
-        ax2.set_ylim([0, 1])
-        ax2.legend()
-
-        if pond_fraction[-1] > 0:
-            if areas_dim != np.array([]):
-                ax3.plot(areas_dim, dimensions)
-            if areas_scatter != np.array([]):
-                ax3.scatter(areas_scatter, dimensions_scatter)
-
-            ax3.set_xscale('log')
-            ax3.set_xlabel('area [m^2]')
-            ax3.set_ylabel('fractal dimension')
-
-        plt.tight_layout()
-        plt.savefig(f"experiments/{experiment_name}/figures/{run[0].replace('.npy', '')}.png", dpi=300)
-
-
-def entropy_v4(z_values,size):
-    unique_values, value_counts = np.unique(z_values, return_counts=True)
-    probabilities = value_counts / np.sum(value_counts)
-    grid_entropy = stats.entropy(probabilities, base=2)
-    entropy_fully_random = 13.287712379549442
-    return grid_entropy * (np.log(100) / np.log(size)) / entropy_fully_random
-
-
-def entropy_v4_noscale(z_values):
-    unique_values, value_counts = np.unique(z_values, return_counts=True)
-    probabilities = value_counts / np.sum(value_counts)
-    return stats.entropy(probabilities, base=2)
-
-
-def draw_3d_CA():
-    sidelen = 15
-    np.random.seed(100)
-
-    # Generate example cellular automaton data
-    H, X, Y = initial_distributions.multi_valley(size=sidelen)
-    h = H * np.random.random()  # water
-    H = H * 2
-    # Create a meshgrid for x, y coordinates
-    x, y = np.meshgrid(np.arange(H.shape[1]), np.arange(H.shape[0]))
-
-    # Flatten the arrays to create bars in the 3D plot
-    x = x.flatten()
-    y = y.flatten()
-    z = np.zeros_like(x)
-    z, _, _ = initial_distributions.multi_valley(size=sidelen)
-    z = z.flatten() * np.random.random() * 1.3
-    dx = dy = 0.8
-    dz = H.flatten()
-    dz2 = h.flatten()
-    fig = plt.figure(figsize=(8, 8))
-
-    ax = fig.add_subplot(111, projection='3d')
-    ax.grid(False)
-    ax.xaxis.set_pane_color((0.0, 0.0, 0.0, 0.0))
-    ax.yaxis.set_pane_color((0.0, 0.0, 0.0, 0.0))
-    ax.zaxis.set_pane_color((0.0, 0.0, 0.0, 0.0))
-
-    plt.axis('off')
-
-    nonzero_indices = dz > 0
-    nonzero_indices2 = dz2 > 0
-
-    # Ice
-    ax.bar3d(x[nonzero_indices], y[nonzero_indices], z[nonzero_indices],
-             dx, dy, dz[nonzero_indices] + .38, color='linen', shade=True, alpha=1)  # +.3 azure, seashell,linen
-    # Water
-    ax.bar3d(x[nonzero_indices2], y[nonzero_indices2], dz[nonzero_indices] + .4,
-             dx, dy, dz2[nonzero_indices2], color='steelblue', shade=True, alpha=.4)
-
-    ax.view_init(elev=30, azim=360 - 37)
-    # ax.view_init(elev=70, azim=360-60)
-    plt.gca().invert_xaxis()
-    # plt.savefig("Ice_column_ca")
-    plt.show()
+    return inflection_list
