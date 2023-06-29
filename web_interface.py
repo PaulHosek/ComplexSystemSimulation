82177afd27af056e44f366da2411c9635069bef6
SesswireD <dlbezema@gmail.com>
2023-06-29 16:13:02 +0200

    Fixed web_interface

--- web_interface.py
+++ web_interface.py
@@ -1,6 +1,6 @@
 """
 In this file, we develop a web interface to illustrate the capabilities of the icing-model and how a
-change in the model-parameters or in the surface topology affects the overall end result.
+change in the model-parameters or in the surface topography affects the overall end result.
 """
 
 import streamlit as st
@@ -10,6 +10,7 @@ from Iceing_model import iceing_model
 from Iceing_model import glauber
 from Iceing_model import perim_area
 from matplotlib import colors
+from topography import Create_Initial_Topography
 
 st.set_page_config(
     page_title="Iceing Model",
@@ -29,7 +30,11 @@ st.title('Iceing Model')
 st.write(
     """ 
     This demo shows the formation of meltponds on arctic sea ice during the summer period using an adaptation of the Ising model.
-    Start by selecting an initial fraction of meltponds and a topology and run the simulation.
+    Start by selecting an initial fraction of meltponds and a topography and run the simulation.
+    
+    The figure on the left shows the current state of the system where blue indicates water and white inidicates ice.
+    The top figure to the right tracks the convergence of the system. It is the sum of the system states (-1,ice;+1,water). Once the sum flatlines, the system is in equilibrium and the simulation can be stopped.
+    The bottom right figure shows the area and perimeter of each meltpond cluster in the system. It is an indication of the fractal dimension.
 
     """
 )
@@ -37,13 +42,13 @@ st.write(
 def plot_state():
     """plots the state of the Iceing system"""
         
-    #display initial topology
+    #display initial topography
     st.session_state.fig = plt.figure(constrained_layout=True)
     st.session_state.axs = st.session_state.fig.subplot_mosaic([['Left', 'TopRight'],['Left', 'BottomRight']],
                         gridspec_kw={'width_ratios':[2, 1]})
     
     #titles
-    st.session_state.axs['Left'].set_title('Melt Pond Topology')
+    st.session_state.axs['Left'].set_title('Melt Pond Topography')
     st.session_state.axs['TopRight'].set_title('Convergence')
     st.session_state.axs['BottomRight'].set_title('Perimeters')
 
@@ -77,20 +82,30 @@ def plot_state():
 with st.sidebar:
     with st.form(key="simulation_param"):
         input_fraction = st.slider('Initial fraction of meltponds', 0.0, 1.0, 0.45)
-        topology = st.selectbox('Initial topology', ["normal","snow dune","diffusion","kayleigh"])
+        topography = st.selectbox('Initial topography', ["normal","snow dune","diffusion","rayleigh"])
         st.session_state.initialize = st.form_submit_button(label="Initialize domain")
         st.session_state.start_sim_clicked = st.form_submit_button(label="Start Simulation")
         st.session_state.stop_sim_clicked = st.form_submit_button(label ="Stop Simulation")
 
 #Initialize domain
 if st.session_state.initialize:
-    with st.spinner('Initialize domain..'):
+    with st.spinner('Initialize domain...'):
         #initialize model
         st.session_state.model = iceing_model(input_fraction,120)
 
         #list for checking convergence values
         st.session_state.convergence = []
         st.session_state.convergence.append(np.sum(st.session_state.model.s))
+
+        if topography == "snow dune":
+            st.session_state.model.hi = Create_Initial_Topography(res=120, mode='snow_dune', tmax=2, dt=0.1, g=1, sigma_h=1., h=0., snow_dune_radius=1.,
+                              Gaussians_per_pixel=0.2, number_of_r_bins=150, window_size=5, snow_dune_height_exponent=1.)
+        elif topography == "diffusion":
+            st.session_state.model.hi = Create_Initial_Topography(res=120, mode='diffusion', tmax=2, dt=0.1, g=1, sigma_h=1., h=0., snow_dune_radius=1.,
+                              Gaussians_per_pixel=0.2, number_of_r_bins=150, window_size=5, snow_dune_height_exponent=1.)
+        elif topography == "rayleigh":
+            st.session_state.model.hi = Create_Initial_Topography(res=120, mode='rayleigh', tmax=2, dt=0.1, g=1, sigma_h=1., h=0., snow_dune_radius=1.,
+                              Gaussians_per_pixel=0.2, number_of_r_bins=150, window_size=5, snow_dune_height_exponent=1.)
 
         #display initial state
         plot_state()
